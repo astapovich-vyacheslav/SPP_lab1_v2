41d7349757b96b5a22023983e1f4af5bdf9621c5
Vyacheslav <astapovich_work@gmail.com>
2022-09-20 00:25:26 +0300

    Добавьте файлы проекта.

---
+++ TracerLib/TraceResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracerLib
+{
+    public class TraceResult
+    {
+        public double Time { get; set; }
+        public string MethodName { get; set; }
+        public string ClassName { get; set; }
+
+        public TraceResult(double time, string methodName, string className)
+        {
+            Time = time;
+            MethodName = methodName;
+            ClassName = className;
+        }
+        public void PrintResult()
+        {
+            Console.WriteLine(Time);
+            Console.WriteLine(MethodName);
+            Console.WriteLine(ClassName);
+        }
+    }
+}
---
+++ TracerLib/Tracer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TracerLib
+{
+    public class Tracer : ITracer
+    {
+        public double Time;
+        public string MethodName, ClassName;
+        public Stopwatch stopwatch { get; set; }
+        public Tracer(string methodName, string className)
+        {
+            MethodName = methodName;
+            ClassName = className;
+        }
+
+        public Tracer() { }
+
+        // вызывается в начале замеряемого метода
+        public void StartTrace()
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+
+        // вызывается в конце замеряемого метода 
+        public void StopTrace()
+        {
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedTicks;
+        }
+
+        // получить результаты измерений  
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult(Time, MethodName, ClassName);
+        }
+    }
+}
